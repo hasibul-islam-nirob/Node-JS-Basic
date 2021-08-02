f63b1cb71f657fc7492f98e82c22a49b432ab775
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-02 14:21:30 +0600

    3

--- first-ex-project/main.js
+++ first-ex-project/main.js
@@ -2,7 +2,18 @@
 var http = require('http');
 
 var myServer = http.createServer(function (request, response){
-    response.end('Hello Programmer');
+
+    if (request.url == "/"){
+        response.writeHead(200, {'Content-Type':'text/html'})
+        response.write('<h1>This is Home Page</h1>')
+        response.end();
+
+    }else if (request.url == "/about"){
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.write('<h1>This is About Page</h1>')
+        response.end();
+    }
+
 });
 
 myServer.listen(2525);
