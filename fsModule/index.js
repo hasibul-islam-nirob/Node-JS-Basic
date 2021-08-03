73b4abc59375b4e74d1272dadabfe91324a68d2a
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-03 16:37:58 +0600

    FS System Asynchronous File Delete

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,34 +4,34 @@ var fs = require('fs');
 var myServer = http.createServer(function (req,res){
     if (req.url == '/'){
 
-    /*
-        //File Rename Asynchronous
-        fs.rename('nirob.txt','nirobNew.txt',function (error){
+        //File Delete Asynchronous
+        fs.unlink('nirobNew.txt',function (error){
            if (error){
                res.writeHead(200,{'Content-Type':'text/html'})
-               res.write("File Rename Fail")
+               res.write("File Delete Fail")
                res.end();
            }else {
                res.writeHead(200,{'Content-Type':'text/html'})
-               res.write("File Rename Success")
+               res.write("File Delete Success")
                res.end();
            }
         })
 
-     */
+        /*
+          //File Delete Synchronous
+             var error = fs.renameSync('nirobSync.txt','nirobSyncNew.txt');
+            if (error){
+               res.writeHead(200,{'Content-Type':'text/html'})
+               res.write("File Delete Fail")
+               res.end();
+           }else {
+               res.writeHead(200,{'Content-Type':'text/html'})
+               res.write("File Delete Success")
+               res.end();
+           }
 
+         */
 
-        //File Rename Synchronous
-        var error = fs.renameSync('nirobSync.txt','nirobSyncNew.txt');
-        if (error){
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write("File Rename Fail")
-            res.end();
-        }else {
-            res.writeHead(200,{'Content-Type':'text/html'})
-            res.write("File Rename Success")
-            res.end();
-        }
 
 
     }
