2d284d810c2c137851c89190161af31072a0359a
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-03 15:36:42 +0600

    Asynchronous FS Modules

--- fsModule/index.js
+++ fsModule/index.js
@@ -2,18 +2,14 @@ var http = require('http');
 var fs = require('fs');
 
 var myServer = http.createServer(function (req,res){
-
     if (req.url == '/'){
-
-        // Asy
+        // Asynchronous
         fs.readFile('home.html',function (error,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end();
         })
-
     }
-
 })
 myServer.listen(2000);
 console.log("Server Run Success");
