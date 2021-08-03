986d804a2c41cd9f76262a7e63c61013e9bcc066
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-03 16:18:34 +0600

    FS System Asynchronous File Rename

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,22 +4,22 @@ var fs = require('fs');
 var myServer = http.createServer(function (req,res){
     if (req.url == '/'){
 
-        /*
-        //File Write Asynchronous
-        fs.writeFile('nirob.txt','Hello nirob.txt file created',function (error){
+
+        //File Rename Asynchronous
+        fs.rename('nirob.txt','nirobNew.txt',function (error){
            if (error){
                res.writeHead(200,{'Content-Type':'text/html'})
-               res.write("File Create Fail")
+               res.write("File Rename Fail")
                res.end();
            }else {
                res.writeHead(200,{'Content-Type':'text/html'})
-               res.write("File Create Success")
+               res.write("File Rename Success")
                res.end();
            }
         })
 
-         */
 
+        /*
         //File Write Synchronous
         var error = fs.writeFileSync('nirobSync.txt','Hello nirobSync.txt file created');
         if (error){
@@ -31,6 +31,8 @@ var myServer = http.createServer(function (req,res){
             res.write("File Create Success")
             res.end();
         }
+
+         */
 
     }
 })
