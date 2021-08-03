9bca2d8cce2d6f10916f5ebeed4ea35e891c3814
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-03 15:45:17 +0600

    Asynchronous FS Modul

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,13 +3,21 @@ var fs = require('fs');
 
 var myServer = http.createServer(function (req,res){
     if (req.url == '/'){
+        /*
         // Asynchronous
         fs.readFile('home.html',function (error,data){
             res.writeHead(200,{'Content-Type':'text/html'})
             res.write(data)
             res.end();
         })
+
+         */
+        // Synchronous
+        var mydata = fs.readFileSync('home.html');
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(mydata);
+        res.end();
     }
 })
-myServer.listen(2000);
+myServer.listen(3000);
 console.log("Server Run Success");
