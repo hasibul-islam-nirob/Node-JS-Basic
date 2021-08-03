79c232c79237bf0345ccb5898826143037037636
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-03 16:07:57 +0600

    FS System Synchronous File Write

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var myServer = http.createServer(function (req,res){
     if (req.url == '/'){
 
+        /*
         //File Write Asynchronous
         fs.writeFile('nirob.txt','Hello nirob.txt file created',function (error){
            if (error){
@@ -17,15 +18,20 @@ var myServer = http.createServer(function (req,res){
            }
         })
 
-
-        /*
-        //File Write Synchronous
-        var mydata = fs.readFileSync('home.html');
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write(mydata);
-        res.end();
-
          */
+
+        //File Write Synchronous
+        var error = fs.writeFileSync('nirobSync.txt','Hello nirobSync.txt file created');
+        if (error){
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write("File Create Fail")
+            res.end();
+        }else {
+            res.writeHead(200,{'Content-Type':'text/html'})
+            res.write("File Create Success")
+            res.end();
+        }
+
     }
 })
 myServer.listen(3000);
