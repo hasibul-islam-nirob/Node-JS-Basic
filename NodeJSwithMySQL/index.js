7c7f3effa115506b3e52169290301897bf209c51
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-08-04 16:53:30 +0600

    Insert Data into MySQL on Node JS

--- NodeJSwithMySQL/index.js
+++ NodeJSwithMySQL/index.js
@@ -9,11 +9,21 @@ var databaseConnection = {
 }
 
 var connection = mysql.createConnection(databaseConnection);
-
 connection.connect(function (error){
     if (error){
         console.log("Connection Fail");
     }else{
         console.log("Connection Success");
+        inserData(connection);
     }
 });
+
+function inserData(connection){
+    let SQL = "INSERT INTO `student`(`name`, `phone`, `code`) VALUES ('Nirob','01957111','1001')";
+    connection.query(SQL, function (error){
+        if (error)
+            console.log("Insert Fail");
+        else
+            console.log("Insert Success");
+    })
+}
